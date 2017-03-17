116f27a290ab071c4b70412bde1d5e8171b30527
dongne <neal.dong@oocl.com>
2017-03-17 11:09:48 +0800

    modify the path of service-workeer.js path :bug:

--- scripts/app.js
+++ scripts/app.js
@@ -345,7 +345,7 @@
   // TODO add service worker code here
   if('serviceWorker' in navigator) {  
       navigator.serviceWorker  
-          .register('/service-worker.js')  
+          .register('service-worker.js')  
           .then(function() { console.log('Service Worker Registered'); });  
   }
 })();
