b505e576e8cc09cb192cf1eafac56d119334806b
neal1991 <neal1991@sina.com>
2017-03-18 11:11:22 +0800

    modify the cache file path

--- service-worker.js
+++ service-worker.js
@@ -3,21 +3,21 @@ var dataCacheName = 'weatherData-v1';
 var filesToCache = [
   '/',
   'index.html',
-  '../scripts/app.js',
-  '../styles/inline.css',
-  '../images/clear.png',
-  '../images/cloudy-scattered-showers.png',
-  '../images/cloudy.png',
-  '../images/fog.png',
-  '../images/ic_add_white_24px.svg',
-  '../images/ic_refresh_white_24px.svg',
-  '../images/partly-cloudy.png',
-  '../images/rain.png',
-  '../images/scattered-showers.png',
-  '../images/sleet.png',
-  '../images/snow.png',
-  '../images/thunderstorm.png',
-  '../images/wind.png'
+  'scripts/app.js',
+  'styles/inline.css',
+  'images/clear.png',
+  'images/cloudy-scattered-showers.png',
+  'images/cloudy.png',
+  'images/fog.png',
+  'images/ic_add_white_24px.svg',
+  'images/ic_refresh_white_24px.svg',
+  'images/partly-cloudy.png',
+  'images/rain.png',
+  'images/scattered-showers.png',
+  'images/sleet.png',
+  'images/snow.png',
+  'images/thunderstorm.png',
+  'images/wind.png'
 ];
 
 self.addEventListener('install', function(e) {
