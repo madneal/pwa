4c8ff90623ebfaa9d85e36cd339ab3f9647e26ff
neal1991 <neal1991@sina.com>
2017-03-18 11:02:31 +0800

    modify the path in service-worker

--- service-worker.js
+++ service-worker.js
@@ -2,22 +2,22 @@ var cacheName = 'weatherPWA-step-6-1';
 var dataCacheName = 'weatherData-v1';
 var filesToCache = [
   '/',
-  '/index.html',
-  '/scripts/app.js',
-  '/styles/inline.css',
-  '/images/clear.png',
-  '/images/cloudy-scattered-showers.png',
-  '/images/cloudy.png',
-  '/images/fog.png',
-  '/images/ic_add_white_24px.svg',
-  '/images/ic_refresh_white_24px.svg',
-  '/images/partly-cloudy.png',
-  '/images/rain.png',
-  '/images/scattered-showers.png',
-  '/images/sleet.png',
-  '/images/snow.png',
-  '/images/thunderstorm.png',
-  '/images/wind.png'
+  'index.html',
+  '../scripts/app.js',
+  '../styles/inline.css',
+  '../images/clear.png',
+  '../images/cloudy-scattered-showers.png',
+  '../images/cloudy.png',
+  '../images/fog.png',
+  '../images/ic_add_white_24px.svg',
+  '../images/ic_refresh_white_24px.svg',
+  '../images/partly-cloudy.png',
+  '../images/rain.png',
+  '../images/scattered-showers.png',
+  '../images/sleet.png',
+  '../images/snow.png',
+  '../images/thunderstorm.png',
+  '../images/wind.png'
 ];
 
 self.addEventListener('install', function(e) {
