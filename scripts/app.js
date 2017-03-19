42f834ac0482c5e954a1667d72864f02ff5d599c
neal1991 <neal1991@sina.com>
2017-03-19 09:23:54 +0800

    delete data in localStorage :bug:

--- scripts/app.js
+++ scripts/app.js
@@ -265,6 +265,8 @@
       if (index !== -1) {        
         app.selectedCities.splice(index, 1);
       }
+      var selectedCities = JSON.stringify(app.selectedCities);
+      localStorage.selectedCities = selectedCities;
     }
   }
 
@@ -345,49 +347,6 @@
         return 'partly-cloudy-day';
     }
   };
-
-  /*
-   * Fake weather data that is presented when the user first uses the app,
-   * or when the user has not saved any cities. See startup code for more
-   * discussion.
-   */
-  var initialWeatherForecast = {
-    key: '2459115',
-    label: 'New York, NY',
-    created: '2016-07-22T01:00:00Z',
-    channel: {
-      astronomy: {
-        sunrise: "5:43 am",
-        sunset: "8:21 pm"
-      },
-      item: {
-        condition: {
-          text: "Windy",
-          date: "Thu, 21 Jul 2016 09:00 PM EDT",
-          temp: 56,
-          code: 24
-        },
-        forecast: [
-          {code: 44, high: 86, low: 70},
-          {code: 44, high: 94, low: 73},
-          {code: 4, high: 95, low: 78},
-          {code: 24, high: 75, low: 89},
-          {code: 24, high: 89, low: 77},
-          {code: 44, high: 92, low: 79},
-          {code: 44, high: 89, low: 77}
-        ]
-      },
-      atmosphere: {
-        humidity: 56
-      },
-      wind: {
-        speed: 25,
-        direction: 195
-      }
-    }
-  };
-  // TODO uncomment line below to test app with fake data
-  // app.updateForecastCard(initialWeatherForecast);
 
   // TODO add startup code here
   app.selectedCities = localStorage.selectedCities;
